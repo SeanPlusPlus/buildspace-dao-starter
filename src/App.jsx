6015507ddabb5ee34f2ddf8a639c458ae5cae4b1
Sean Stephenson <seanmichaelstephenson@gmail.com>
2021-12-25 13:16:41 -0800

    update react scr

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useWeb3 } from "@3rdweb/hooks";
 
 const App = () => {
+  const [name, setName] = useState('VeloDAO');
   // Use the connectWallet hook thirdweb gives us.
   const { connectWallet, address, error, provider } = useWeb3();
   console.log("👋 Address:", address)
@@ -13,7 +14,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to {name}</h1>
         <button onClick={() => connectWallet("injected")} className="btn-hero">
           Connect your wallet
         </button>
